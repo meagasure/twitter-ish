ba2c468ac2dc5ee7decf8805b120e2eb557a2700
S Erinle <erinle.oav@gmail.com>
2017-02-17 18:50:53 -0500

    Setup of Table View Class for reuse

---
+++ Twitter/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  homeController.swift
+//  Twitter
+//
+//  Created by Oluwaseun Erinle on 2/17/17.
+//  Copyright © 2017 megasure. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+    }
+}
